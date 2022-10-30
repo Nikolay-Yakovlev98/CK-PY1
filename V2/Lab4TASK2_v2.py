c0160784f2625a7b187f70a2d6c07508b907c9bd
Nikolay-Yakovlev98 <115218755+Nikolay-Yakovlev98@users.noreply.github.com>
2022-10-30 20:16:20 +0300

    Лабораторная работа №4 (попытка 2)

---
+++ V2/Lab4TASK2_v2.py
@@ -0,0 +1,30 @@
+def get_count_char(str_):
+    str_ = str_.lower()
+    slovar = {}
+    for bukva in str_:
+        if bukva.isalpha():
+            if bukva in slovar:
+                slovar[bukva] += 1
+            else:
+                slovar[bukva] = 1
+
+    return slovar
+
+def dictionary (dict_):
+    dict2 = dict_
+    summa = sum(dict2.values())
+    for letter in dict2:
+        dict2[letter] = (dict2[letter] * 100) / summa
+
+
+    return dict2
+
+
+
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+print(get_count_char(main_str))
+
